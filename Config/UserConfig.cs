00be5e4a5d1ddc8ce926b30465cc8679a6e140ce
tengfei8771 <532050072@qq.com>
2019-07-19 11:05:34 +0800

    1.修改bug

--- Config/UserConfig.cs
+++ Config/UserConfig.cs
@@ -8,7 +8,7 @@ namespace WeChatPlatform.Config
     {
         public static readonly string appid = "wx3797271fa86710d0";//用户Appid
         public static readonly string secret = "5f13c6cc3b564d5fd3c09df5351d5eb4";//用户appsecret
-        public static string token = "23_hcrIvMrkCIePXSGoxwxG5L209tNAVwfhMrTfwDgSC8JpOVX2YH3oFqKB2BbyQZvXFX1aheLH2P9hMWkaBE3agluQVIzBId9W4JeMHAJy7VrhbKGy_-IoN0FxiZAWVizDJ8OYJ4Vp9pR3xaeXFUAeAEAVKQ";
+        public static string token = "23_QvFxp02eDyk2UePCh1W4Ah4azi1z_Jsl4ZzOpIxCuqsGzCkm5fsVvMSB38ZTklkCI4JS9c96EvVjkS--LsJ3gUsIHKYdwdpds6lNRyKY3EdB01j9sS4LyKNVGPaJtFxqPiOZjprTqNXsndEgXISdAFAEPZ";
         public static double expireTime;
 
     }
